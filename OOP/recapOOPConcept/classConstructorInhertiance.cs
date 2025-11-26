290eb2c9d49af049c3c1b45ec8095d166ca4e856
raihanuldev <rihanthedev@gmail.com>
2025-11-26 12:44:11 +0600

    inheritance exmaple flow catch

--- OOP/recapOOPConcept/classConstructorInhertiance.cs
+++ OOP/recapOOPConcept/classConstructorInhertiance.cs
@@ -17,6 +17,13 @@ class Bag
     }
 
 }
+
+class LifeStyle : Bag
+{
+    string tshrrtBrand ="XYS";
+    
+}
+
 class Practice
 {
     static void Main(string[] args)
@@ -29,5 +36,10 @@ class Practice
         // this example showcase class constrctor, Method OVerloading.
         Console.WriteLine("The Bag Company of b1=> "+b1.Company +" and the color of "+b1.Color);
         Console.WriteLine("The Bag Company of b2=> "+b2.Company +" and the color of "+b2.Color);
+
+        // this example showcase of Inheritance 
+        LifeStyle raihan = new LifeStyle();
+        raihan.Color = "pink";
+        Console.WriteLine("bag color of raihan alom is=> "+ raihan.Color);
     }
 }
--- obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9175229ed87a30e8c833b2781f51bda51f55d08d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d4acd3c567c0e3a0c77bdd924cff79823f1d21d7")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
